530a64436b34e1d2a2366cc0f8fc0d97ebfe92ff
Deep2410 <deep24102410@gmail.com>
2024-10-21 18:10:35 -0400

    contact me page design

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -1,9 +1,98 @@
-import React from 'react'
+import React, { useState } from 'react'
+import '../css/contact.css'
 
 function Contact(){
+    const [email, setEmail] = useState('');
+    const [message, setMessage] = useState('');
+    const [name, setName] = useState('');
+    const [subject, setSubjct] = useState('');
+
+    const[receivingEmail, setReceivingEmail] = useState('dm.prajapati8585@gmail.com');
+
+    const handleName = (e) => {
+        setName(e.target.value);
+    }
+
+    const handleEmail = (e) => {
+        setEmail(e.target.value);
+    }
+
+    const handleMessage = (e) => {
+        setMessage(e.target.value);
+    }
+
+    const handleSubject = (e) => {
+        setSubjct(e.target.vlaue);
+    }
+
+
     return(
         <>
-        Contact page
+            <div className="container">
+                <div className="row">
+                    <h1 className='text-center mt-5'>Contact Me</h1>
+                </div>
+
+                <div className="row mt-2">
+                    <h6 className='text-center'>If you have any question you can send me an email using this form.</h6>
+                </div>
+
+                <div className="row mt-5">
+                    <div className="col-sm-6">
+                        <div className="form"> 
+                            <h3>Send Me a Message</h3>
+                            <label htmlFor="name" className="form-label">Your Name</label>
+                            <input type='text' 
+                                id='name' 
+                                placeholder='Enter your name' 
+                                className='form-control'
+                                value={name}
+                                onChange={handleName}
+                                required
+                            />
+
+                            <label htmlFor="email" className="form-label mt-4">Your Email</label>
+                            <input type='email' 
+                                id='email' 
+                                placeholder='Enter your email' 
+                                className='form-control'
+                                value={email}
+                                onChange={handleEmail}
+                            />
+
+                            <label htmlFor="subject" className="form-label mt-4">Subject of the email</label>
+                            <input type='text' 
+                                id='subject' 
+                                placeholder='Subject of the email' 
+                                className='form-control'
+                                value={subject}
+                                onChange={handleSubject}
+                            />
+
+                            <label htmlFor="message" className="form-label mt-4">Enter Message</label>
+                            <textarea type='text' 
+                                id='message' 
+                                placeholder='Enter your Message' 
+                                className='form-control'
+                                value={message}
+                                rows = "5"
+                                onChange={handleMessage}
+                            />
+
+                            <div className="btn btn-primary mt-5 "> Send Email</div>
+
+                        </div>
+                    </div>
+                    <div className="col-sm-6 rightside_contact">
+                        <h3>You are sending an email to </h3>
+                        <p className='mb-0'>Deep Prajapati</p>
+                        <p className='mb-0'>dm.prajapati8585@gmail.com</p>
+                        <p>Contact Number: +1 (437) 335-    2299</p>
+                    </div>
+                    
+                </div>
+
+            </div>
         </>
     )
 
