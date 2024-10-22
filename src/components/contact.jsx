3e461ae73a0b2608f454f1a2f746e5c0ab6a69e6
Deep2410 <deep24102410@gmail.com>
2024-10-22 19:13:37 -0400

    greetings after sending email

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -7,6 +7,7 @@ function Contact(){
     const [message, setMessage] = useState('');
     const [name, setName] = useState('');
     const [emailInProgress, setEmailInProgress] = useState(false);
+    const [showGreetings, setShowGreetings] = useState(false);
     
     const[receivingEmail, setReceivingEmail] = useState('dm.prajapati8585@gmail.com');
 
@@ -35,10 +36,13 @@ function Contact(){
                 // http://localhost:8000/send-email
     
                 if(response.status === 200){
-                    alert('Email sent sucscessfully');
                     setEmail('');
                     setMessage('');
-                    setName('')
+                    setName('');
+                    setShowGreetings(true);
+                    setTimeout(() => {
+                        setShowGreetings(false);
+                    },5000);
                 }
             }
             catch(error){
@@ -53,73 +57,82 @@ function Contact(){
 
     return(
         <>
-            <div className="container card contact-container">
-                <div className="row">
-                    <h1 className='text-center '>Contact Me</h1>
-                </div>
-
-                <div className="row mt-2">
-                    <h6 className='text-center'>If you have any question you can send me an email using this form.</h6>
-                </div>
-
-                <div className="row mt-5">
-                    <div className="col-sm-6">
-                        <div className="form"> 
-                            <h3>Send Me a Message</h3>
-                            <label htmlFor="name" className="form-label">Your Name</label>
-                            <input type='text' 
-                                id='name' 
-                                placeholder='Enter your name' 
-                                className='form-control'
-                                value={name}
-                                onChange={handleName}
-                                required
-                            />
-
-                            <label htmlFor="email" className="form-label mt-4">Your Email</label>
-                            <input type='email' 
-                                id='email' 
-                                placeholder='Enter your email' 
-                                className='form-control'
-                                value={email}
-                                onChange={handleEmail}
-                            />
-
-                            <label htmlFor="message" className="form-label mt-4">Enter Message</label>
-                            <textarea type='text' 
-                                id='message' 
-                                placeholder='Enter your Message' 
-                                className='form-control'
-                                value={message}
-                                rows = "5"
-                                onChange={handleMessage}
-                            />
-                            {
-                                emailInProgress ?
-                                (
-                                    <div className='mt-5 loading-text-container'>
-                                        <div class="spinner-border text-primary" role="status">
-                                            <span class="sr-only"></span>
-                                        </div>
-                                        <div className='loading-text'><p>Sending Email...</p></div>
-                                    </div>
-                                     ) : (<div className="btn btn-primary mt-5" onClick={sendEmail}> Send Email</div>)
-                                } 
-
-                            
-
+            {
+                showGreetings ? (
+                    <div className='container card greeting_container'>
+                        <h1 className='text-center'>Thank you for contacting me.</h1>
+                    </div>
+                ) :(
+                    <div className="container card contact-container">
+                        <div className="row">
+                            <h1 className='text-center '>Contact Me</h1>
                         </div>
+        
+                        <div className="row mt-2">
+                            <h6 className='text-center'>If you have any question you can send me an email using this form.</h6>
+                        </div>
+        
+                        <div className="row mt-5">
+                            <div className="col-sm-6">
+                                <div className="form"> 
+                                    <h3>Send Me a Message</h3>
+                                    <label htmlFor="name" className="form-label">Your Name</label>
+                                    <input type='text' 
+                                        id='name' 
+                                        placeholder='Enter your name' 
+                                        className='form-control'
+                                        value={name}
+                                        onChange={handleName}
+                                        required
+                                    />
+        
+                                    <label htmlFor="email" className="form-label mt-4">Your Email</label>
+                                    <input type='email' 
+                                        id='email' 
+                                        placeholder='Enter your email' 
+                                        className='form-control'
+                                        value={email}
+                                        onChange={handleEmail}
+                                    />
+        
+                                    <label htmlFor="message" className="form-label mt-4">Enter Message</label>
+                                    <textarea type='text' 
+                                        id='message' 
+                                        placeholder='Enter your Message' 
+                                        className='form-control'
+                                        value={message}
+                                        rows = "5"
+                                        onChange={handleMessage}
+                                    />
+                                    {
+                                        emailInProgress ?
+                                        (
+                                            <div className='mt-4 loading-text-container'>
+                                                <div class="spinner-border text-primary" role="status">
+                                                    <span class="sr-only"></span>
+                                                </div>
+                                                <div className='loading-text'><p>Sending Email...</p></div>
+                                            </div>
+                                            ) : (<div className="btn btn-primary mt-4" onClick={sendEmail}> Send Email</div>)
+                                        } 
+        
+                                    
+        
+                                </div>
+                            </div>
+                            <div className="col-sm-6 rightside_contact">
+                                <h3>You are sending an email to </h3>
+                                <p className='mb-0'>Deep Prajapati</p>
+                                <p className='mb-0'>dm.prajapati8585@gmail.com</p>
+                                <p>Contact Number: +1 (437) 335-2299</p>
+                            </div>
+                            
+                        </div>
+        
                     </div>
-                    <div className="col-sm-6 rightside_contact">
-                        <h3>You are sending an email to </h3>
-                        <p className='mb-0'>Deep Prajapati</p>
-                        <p className='mb-0'>dm.prajapati8585@gmail.com</p>
-                        <p>Contact Number: +1 (437) 335-2299</p>
-                    </div>
-                    
-                </div>
-
-            </div>
+                )
+            }
+         
         </>
     )
 
