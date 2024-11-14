59f5f96878e7fc1d6c40fb05c70d3dd040b06f7a
Deep2410 <deep24102410@gmail.com>
2024-11-13 20:00:20 -0500

    fixed existing user

--- src/components/Authentication.jsx
+++ src/components/Authentication.jsx
@@ -10,15 +10,16 @@ function Authentication() {
     const [inProgress, setInProgress] = useState(false);
     const [message, setMessage] = useState('');
     const navigate = useNavigate();
+    const [tokenMessage, setTokenMessage] = useState('');
+    const [tockenAvailable, setTockenAvailable] = useState(false);
+    const [userID, setUserId] = useState(0);
 
     useEffect(() => {
         const token = localStorage.getItem('token');
-
         if(!token){
-            setMessage('Token not found! please log in.');
+            setTokenMessage('Token not found! please log in.');
             return;
         }
-
         // http://localhost:8000/contact
         // https://resume-backend-9i7r.onrender.com/contact
 
@@ -27,9 +28,13 @@ function Authentication() {
                 'Authorization': `Bearer ${token}`
             }
         }).then(response => {
+            const user_id = response.data.userId;
             if(response.status === 200){
-                navigate('/projects/auth/home');
+                // navigate('/projects/auth/home',{state: {user_id} });
+                setUserId(user_id)
+                setTockenAvailable(true);
             }
+            console.log(response.data.userId);
             console.log(response.data.message);
         }).catch(error => {
             console.error('Error in useEffect hook: ' + error);
@@ -47,21 +52,28 @@ function Authentication() {
             if(response.status === 200){
                 console.log(response.data.message);
                 const token = response.data.token;
+                const user_id = response.data.user_id;
+                console.log(user_id);
                 localStorage.setItem('token',token);
                 console.log(token)
-                navigate('/projects/auth/home');
+                navigate('/projects/auth/home', {state: {user_id}});
             }
         }
         catch(error){
             setMessage(error.response.data.message);
         }
         setInProgress(false);
-
     }
 
+    const navigateToHome = () => {
+        if(tockenAvailable){
+            console.log(userID);
+            navigate('/projects/auth/home',{state: {user_id: userID} });
+        }
+    }
+    
     return(
         <>  
-        
             <div className="bg-light d-flex justify-content-center align-items-center vh-100">
             <div className="card p-4 shadow-sm form_container">
                 <h2 className="text-center mb-4 login_heading">Login</h2>
@@ -104,14 +116,20 @@ function Authentication() {
                     </div>
                     <div className="mt-3 text-center">
                         <Link to="/projects/auth/signup" className="text-decoration-none">New User? Register here</Link>
+                        {tockenAvailable ? 
+                            <div>
+                                <p>You are already Loged In!! Click on button below to log in</p>
+                                <button className='btn btn-primary' onClick={navigateToHome} type='button'>Existing User</button>
+                            </div> :
+                         ''}
                     </div>
                 </form>
             </div>
-        </div>
-        
+
             
+        </div>    
+        
         </>
     )
 }
-
 export default Authentication;
--- src/components/login_home.jsx
+++ src/components/login_home.jsx
@@ -1,16 +1,49 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, {useState, useEffect} from 'react';
+import { useNavigate, useLocation } from 'react-router-dom';
+import axios from 'axios';
 
 function Login_home(){
-
     const navigate = useNavigate();
+    const location = useLocation();
+    const user_id = location.state?.user_id;
+    const [userData, setUserData] = useState(null);
     const logout = () => {
         localStorage.removeItem('token');
         navigate('/projects/auth');
     }
+
+    useEffect(() => {
+    
+        const fetchUserData = async () => {
+            try {
+                if (user_id) {
+                    // Await the axios request
+                    // http://localhost:8000/user/${user_id}
+                    // https://resume-backend-9i7r.onrender.com/user/${user_id}
+                    const response = await axios.get(`https://resume-backend-9i7r.onrender.com/user/${user_id}`);
+                    setUserData(response.data);
+                }
+            } catch (error) {
+                console.error(`Error in fetching user data: ${error}`);
+            }
+        };
+
+        fetchUserData();
+    },[user_id]);
+
+    
     return(
         <>
             This is the log in home page<br></br>
+            UserId: {user_id}<br></br>
+            {userData ? (
+                <>
+                    <div>User Name: {userData.name}</div>
+                    <div>User Email: {userData.email}</div>
+                </>
+            ) : (
+                <div>Loading user data...</div>
+            )}
             <button onClick={logout} className='btn btn-primary'> Log out</button>
         </>
     )
